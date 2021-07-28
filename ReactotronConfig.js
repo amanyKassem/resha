85fdf9e0e55d67c185f674e97d600da4bb4ea36e
amanyKassem <amanykassem@Amany.local>
2021-07-28 13:00:46 +0200

    map new key

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-import Reactotron from 'reactotron-react-native';import { reactotronRedux } from 'reactotron-redux';const reactotron = Reactotron.configure({ name: 'resha', host : '192.168.1.33', port: 9090 })							 .use(reactotronRedux()).useReactNative()							 .connect();export default reactotron
-=======
-import Reactotron from 'reactotron-react-native';import { reactotronRedux } from 'reactotron-redux';const reactotron = Reactotron.configure({ name: 'resha', host : '192.168.2.6', port: 9090 })							 .use(reactotronRedux()).useReactNative()							 .connect();export default reactotron
->>>>>>> fef962f7ba6db4dabf1a8d39476c79cb23445ae6
+import Reactotron from 'reactotron-react-native';import { reactotronRedux } from 'reactotron-redux';const reactotron = Reactotron.configure({ name: 'resha', host : '192.168.2.6', port: 9090 })							 .use(reactotronRedux()).useReactNative()							 .connect();export default reactotron
